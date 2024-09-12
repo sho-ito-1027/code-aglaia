0ece4e6a91d4248d8b4631217dcc8e059027e74b
aryzae <a.aryzae@gmail.com>
2024-09-12 17:40:48 +0900

    feat: add project

---
+++ Aglaia/Aglaia/AglaiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AglaiaApp.swift
+//  Aglaia
+//
+//  Created by aryzae on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct AglaiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ Aglaia/Aglaia/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aglaia
+//
+//  Created by aryzae on 2024/09/12.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
